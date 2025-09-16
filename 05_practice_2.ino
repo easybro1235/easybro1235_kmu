6fba793112c25d012a35040b0bca5eede9dcaa7b
easybro1235 <easybro1235@kookmin.ac.kr>
2025-09-16 23:36:29 +0900

    Add files via upload

---
+++ 05_practice_2.ino
@@ -0,0 +1,18 @@
+#define ledPin 7
+
+void setup() {
+  pinMode(ledPin, OUTPUT);  
+}
+
+void loop() {
+  digitalWrite(ledPin, 0);
+  delay(1000);
+  for(int i = 0; i < 5; i++) {
+    digitalWrite(ledPin, 1);
+    delay(100);
+    digitalWrite(ledPin, 0);
+    delay(100);
+  }
+  digitalWrite(ledPin, 1);
+  while(1) {};
+}
